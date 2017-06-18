e3145e6c19f630539a857e20f173799502cce3e2
Andrzej Kysorets <andrzej.kysorets@o2.pl>
2017-06-18 21:34:12 +0200

    Change wsgi

--- shaman/wsgi.py
+++ shaman/wsgi.py
@@ -8,7 +8,12 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import signal
 
+import sys
+import traceback
+
+import time
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
